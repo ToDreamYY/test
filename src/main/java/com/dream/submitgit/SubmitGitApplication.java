bce2f68a3897e9980a36fc67cc0d6e8d1c0693ae
ToDreamYY <16601716505@163.com>
2019-02-18 14:48:13 +0800

    第一次

---
+++ src/main/java/com/dream/submitgit/SubmitGitApplication.java
@@ -0,0 +1,13 @@
+package com.dream.submitgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmitGitApplication.class, args);
+    }
+
+}
